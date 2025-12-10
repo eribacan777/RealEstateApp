86f4cf2b3d1237dd5710f7257741d03cf35467c2
GligorMaria <mariaandreeagligor@gmail.com>
2025-12-10 10:21:09 +0200

    .

--- ClientApp/obj/Debug/net9.0-windows/ClientApp.AssemblyInfo.cs
+++ ClientApp/obj/Debug/net9.0-windows/ClientApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04a01a848dab460d47b350f6e7161ba9e8161728")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a1c29ba7e4ea1262051043d37b1489b4ea1a9aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
